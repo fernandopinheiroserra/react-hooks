7f60025f7877cd374184e97d1b711deb68a9696c
fernandopinheiroserra <fernando.serra@alternativasistemas.com.br>
2022-06-03 17:11:42 -0300

    useReducer

---
+++ react-hooks/src/App_useEffec_useState.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { FaStar } from "react-icons/fa";
+
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+    .then(response => response.json())
+    .then(setData);
+  })
+//on going!!!
+  if (data) {
+    return (
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  return <p>No Users</p>;
+}
+  
+  //Check!
+  //const [checked, setChecked] = useState(false);
+
+  //return (
+  //  <div className="App">
+  //    <input type="checkbox" value={checked} onChange={() => setChecked((checked) => !checked)}/>
+  //    <p>{checked ? "yep" : "nope"}</p>
+  //  </div>
+  //);
+
+
+export default App;
